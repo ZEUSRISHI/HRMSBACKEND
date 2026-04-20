5da069c25d1ddcc01ad8aae1c099cf614e6fbc9d
RISHIKESHAN K <124979290+ZEUSRISHI@users.noreply.github.com>
2026-04-20 10:49:08 +0530

    Update allowed origin for CORS configuration

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
    ============================================================ */
 
 const allowedOrigins = [
-  "https://hrmspage.netlify.app"
+  "https://hrmsquibo.netlify.app"
 ];
 
 app.use(cors({
