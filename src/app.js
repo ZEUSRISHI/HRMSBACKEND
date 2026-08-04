0ef83ed9c0050af2cd5e10b744dd2f6760b9e952
RISHIKESHAN K <124979290+ZEUSRISHI@users.noreply.github.com>
2026-08-04 11:09:33 +0530

    Remove geolocation from Permissions-Policy header

--- src/app.js
+++ src/app.js
@@ -108,7 +108,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     "Permissions-Policy",
-    "camera=(), microphone=(), geolocation=(), payment=()"
+    "camera=(), microphone=(), payment=()"
   );
   next();
 });
